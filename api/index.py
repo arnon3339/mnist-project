de0535f66888f2982038ba37ea13330d50fa7e2f
arnon3339 <arnon3339@gmail.com>
2025-02-04 23:55:55 +0700

    from local

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from PIL import Image
+import numpy as np
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
@@ -6,3 +8,20 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.post("/api/py/testimg")
+async def predict_emojis(request: Request):
+    body = await request.json()
+    img_data = body['array']
+    # img_arr = np.array(
+    #     list(dict(sorted(img_data.items(),
+    #                      key=lambda x: int(x[0]))).values()),
+    #     dtype=np.uint8)
+    img_arr = np.array(
+        list(dict(sorted(img_data.items(),
+                         key=lambda x: int(x[0]))).values()),
+        dtype=np.uint8).reshape((28, 28))
+    # print(img_arr)
+    img = Image.fromarray(img_arr, mode="L")
+    img.save("./xxx.png")
+    return {"predict": 0}
---
+++ components/image-upload.tsx
@@ -0,0 +1,122 @@
+'use client';
+
+import Img from "next/image";
+import { useEffect, useState } from "react";
+import pica from "pica";
+
+export default function ImageUpload() {
+    const [file, setFile] = useState<string | undefined>(undefined);
+    const [imageArray, setImageArray] = useState<Uint8Array | null>(null);
+    const [predict, setPredict] = useState<number | undefined>(undefined);
+    const picaInstance = pica();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length != 0) {
+            const file = e.target.files?.[0];
+            setFile(URL.createObjectURL(file));
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(file); // Read file as binary
+
+            const targetWidth = 28;  // Desired resized width
+            const targetHeight = 28; // Desired resized height
+
+            reader.onload = () => {
+                            const img = new Image();
+            img.src = URL.createObjectURL(file) ;
+            img.crossOrigin = "Anonymous"; // Prevent CORS issues
+
+            img.onload = async () => {
+                // Create source and destination canvases
+                const srcCanvas = document.createElement("canvas");
+                srcCanvas.width = img.width;
+                srcCanvas.height = img.height;
+                const srcCtx = srcCanvas.getContext("2d");
+                srcCtx?.drawImage(img, 0, 0);
+
+                const destCanvas = document.createElement("canvas");
+                destCanvas.width = targetWidth;
+                destCanvas.height = targetHeight;
+
+                // Use Pica.js for Lanczos resampling
+                await picaInstance.resize(srcCanvas, destCanvas);
+
+                const ctx = destCanvas.getContext("2d");
+                if (ctx) {
+                    const imageData = ctx.getImageData(0, 0, targetWidth, targetHeight);
+                    const pixelArray = imageData.data;
+
+                    // Convert to grayscale
+                    const grayArray = new Uint8Array(targetWidth * targetHeight);
+                    for (let i = 0; i < pixelArray.length; i += 4) {
+                        const r = pixelArray[i];
+                        const g = pixelArray[i + 1];
+                        const b = pixelArray[i + 2];
+
+                        const gray = Math.round(0.299 * r + 0.587 * g + 0.114 * b);
+                        grayArray[i / 4] = gray;
+                    }
+                        // setGrayData({ width: targetWidth, height: targetHeight, array: grayArray });
+                        setImageArray(grayArray)
+                        // console.log("Resized Grayscale Array:", grayArray);
+                    }
+                }; 
+            };
+
+            reader.onerror = () => {
+                console.error("Error reading file.");
+            }; 
+        }
+        else {
+            return
+        }
+    }
+
+    const getPredict= async(e: any) => {
+        e.preventDefault();
+        const res = await fetch('api/py/testimg', {
+            method: "POST",
+            body: JSON.stringify({
+                array: imageArray 
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+        },
+        });
+        if (res.ok) {
+            const resData = await res.json();
+            console.log(resData);
+            setPredict(resData.predict)
+        }
+    }
+
+   return (
+        <div className="flex flex-col gap-y-4">
+            <h2>Add Image:</h2>
+            <input type="file" onChange={handleChange} />
+            <div className="flex flex-col justify-center items-center gap-y-2">
+                {file &&
+                <>
+                    <Img
+                        src={file}
+                        layout="intrinsic"
+                        alt="test image"
+                        width={500}
+                        height={0}
+                        // style={{ width: "auto", height: "auto" }}
+                    />
+                    <button className="text-2xl font-bold border-2 border-foreground px-4 py-2 rounded-lg hover:bg-gray-800"
+                        onClick={getPredict}>
+                        Predict
+                    </button>
+                    <div className="text-2xl w-10 h-10 text-center">
+                        {(predict != undefined)? predict : ""}
+                    </div>
+                </>
+                }
+                {!file &&
+                    <div className="block w-[500px] h-[500px] bg-transparent"></div>
+                }
+            </div>
+        </div>
+   );
+};
