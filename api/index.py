948d9b114d8d5c8b365cbe13cca7a1380babe7e6
arnon <arnon3339@gmail.com>
2025-02-05 19:01:20 +0700

    using onnxruntime

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
-from PIL import Image
 import numpy as np
-from tensorflow import keras
+import onnxruntime as ort
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
@@ -15,10 +14,13 @@ async def predict_digit(request: Request):
     body = await request.json()
     img_arr = np.array(body['array'], dtype=np.uint8)
     img_arr = img_arr / 255.
-    single_image = img_arr.reshape(1, 28, 28, 1)
-    model = keras.models.load_model("./api/model/mnist_cnn.keras")
-    prediction = model.predict(single_image)
-    predicted_class = np.argmax(prediction, axis=1)[0]
+    single_image = img_arr.reshape(1, 28, 28, 1).astype(np.float32)
+
+    session = ort.InferenceSession("./api/model/mnist_cnn.onnx")
+    input_name = session.get_inputs()[0].name
+    output_name = session.get_outputs()[0].name
+    output = session.run([output_name], {input_name: single_image})
+    predicted_class = np.argmax(output[0])
 
     return {"predict": int(predicted_class)}
 
